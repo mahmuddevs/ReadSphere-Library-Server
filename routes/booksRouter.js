59ce54c04ada9a8f5707516e318ff3d44bcc519f
Mahmud Rashid <srabon5704@gmail.com>
2024-12-25 00:52:58 +0600

    added update books functionality

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -17,8 +17,20 @@ bookRouter.get('/all-books', verifyToken, async (req, res) => {
     }
 })
 
+bookRouter.post('/add-book', async (req, res) => {
+    const data = req.body
+    try {
+        const result = await booksCollection.insertOne(data)
+        res.send(result)
+    }
+    catch (err) {
+        res.status(501).send({ message: "Server Side Error" })
+    }
+
+})
+
 bookRouter.post('/single-book', async (req, res) => {
-    const id = req.body
+    const { id } = req.body
     const query = { _id: new ObjectId(id) }
     try {
         const book = await booksCollection.findOne(query)
@@ -43,17 +55,35 @@ bookRouter.post('/book-by-category', async (req, res) => {
 });
 
 
-bookRouter.post('/add-book', async (req, res) => {
-    const data = req.body
-    console.log(data)
+bookRouter.put('/update-book/:id', async (req, res) => {
+    const { id } = req.params
+    const updateData = req.body
+
+    const { image, name, quantity, author, category, rating } = updateData
+
+    const filter = { _id: new ObjectId(id) };
+
     try {
-        const result = await booksCollection.insertOne(data)
+        const options = { upsert: true };
+
+        const updateDoc = {
+            $set: {
+                image,
+                name,
+                quantity,
+                author,
+                category,
+                rating
+            },
+        };
+
+        const result = await booksCollection.updateOne(filter, updateDoc, options);
         res.send(result)
-    }
-    catch (err) {
+    } catch (err) {
         res.status(501).send({ message: "Server Side Error" })
     }
-
 })
+
+
 
 export default bookRouter
